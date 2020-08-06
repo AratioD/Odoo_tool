f8de7ee6b8f41fba9e17b8501e97654064736ea0
AratioD <fffb@ddd.com>
2020-08-06 17:20:27 +0300

    gets closer

--- odoo.py
+++ odoo.py
@@ -3,7 +3,21 @@ import xml.etree.ElementTree as ET
 
 def main():
     tree = ET.parse('country_data.xml')
+    parser = ET.XMLPullParser(['start', 'end'])
+    parser.feed('<rank>sometext')
     root = tree.getroot()
+    
+    print(root.tag)
+    print(root[0][1].text)
+    for child in root:
+        print(child.tag, child.attrib)
+        print(root[2][0].text)
+    
+    for country in root.findall('country'):
+        rank = country.find('rank').text
+        name = country.get('name')
+        print(name, rank)
+
 
 
 if __name__ == "__main__":
